aff5eb0483090abac89570a517f074f3f2cc8b79
PA Ulander <pa@kabelkultur.se>
2025-11-23 02:33:55 +0100

    add type definitions, re-export color, css, config types

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Type definitions for ColorBuddy extension.
+ * 
+ * This module re-exports all type definitions used throughout the extension.
+ */
+
+export * from './color';
+export * from './css';
+export * from './config';
