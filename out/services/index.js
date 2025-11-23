968c765bda3ea3b268cbf7a4926664895059df37
PA Ulander <pa@kabelkultur.se>
2025-11-23 03:50:58 +0100

    add colorParser test map file

--- out/services/index.js
+++ out/services/index.js
@@ -4,11 +4,13 @@
  * Re-exports all service classes
  */
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.StateManager = exports.Cache = exports.Registry = void 0;
+exports.ColorParser = exports.StateManager = exports.Cache = exports.Registry = void 0;
 var registry_1 = require("./registry");
 Object.defineProperty(exports, "Registry", { enumerable: true, get: function () { return registry_1.Registry; } });
 var cache_1 = require("./cache");
 Object.defineProperty(exports, "Cache", { enumerable: true, get: function () { return cache_1.Cache; } });
 var stateManager_1 = require("./stateManager");
 Object.defineProperty(exports, "StateManager", { enumerable: true, get: function () { return stateManager_1.StateManager; } });
+var colorParser_1 = require("./colorParser");
+Object.defineProperty(exports, "ColorParser", { enumerable: true, get: function () { return colorParser_1.ColorParser; } });
 //# sourceMappingURL=index.js.map
