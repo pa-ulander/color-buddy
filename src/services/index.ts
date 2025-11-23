1e23cfc276a0d0b09e142e44e0ee0b6b4560132c
PA Ulander <pa@kabelkultur.se>
2025-11-23 03:35:15 +0100

    add services module, re-export Registry, Cache, StateManager

---
+++ src/services/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Services Module
+ * Re-exports all service classes
+ */
+
+export { Registry } from './registry';
+export { Cache } from './cache';
+export { StateManager } from './stateManager';
