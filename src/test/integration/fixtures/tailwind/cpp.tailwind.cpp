e720bd8376a171db07c660d60eb80990680bb860
PA Ulander <pa@kabelkultur.se>
2025-11-25 01:48:48 +0100

    add cpp.tailwind.cpp fixture

---
+++ src/test/integration/fixtures/tailwind/cpp.tailwind.cpp
@@ -0,0 +1,8 @@
+#include <string>
+#include <iostream>
+
+int main() {
+  std::string classes = "bg-primary hover:bg-primary focus:text-accent text-accent from-primary via-accent to-accent";
+  std::cout << classes;
+  return 0;
+}
