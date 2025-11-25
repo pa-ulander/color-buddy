124fbcd6a078bb70d06976f0c114aafe80d899b7
PA Ulander <pa@kabelkultur.se>
2025-11-25 01:48:22 +0100

    add color snippets PHP fixture

---
+++ src/test/integration/fixtures/php/color-snippets.php
@@ -0,0 +1,12 @@
+<?php
+$primaryHex = '#FF5733';
+$secondaryRgb = "rgb(34, 139, 34)";
+$accentHsl = 'hsl(200 100% 50%)';
+
+$inlineStyles = "<div style='background-color: #1e90ff; color: rgba(255, 255, 255, 0.75);'>Hello World</div>";
+
+$complexTemplate = <<<HTML
+<section class="hero" style="border-color: #ABCDEF;">
+    <p>Accent tone: 240 100% 50%</p>
+</section>
+HTML;
