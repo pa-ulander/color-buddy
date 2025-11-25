7de49f5f508b0130fd54aa8b43f7e32dfe14c0f8
PA Ulander <pa@kabelkultur.se>
2025-11-25 01:49:49 +0100

    add java.tailwind.java fixture

---
+++ src/test/integration/fixtures/tailwind/java.tailwind.java
@@ -0,0 +1,6 @@
+class TailwindClasses {
+  void render() {
+    String classes = "bg-primary hover:bg-primary focus:text-accent text-accent from-primary via-accent to-accent";
+    System.out.println(classes);
+  }
+}
