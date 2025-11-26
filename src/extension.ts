05044031a11c185adf3ba01bbc836697e7284725
PA Ulander <pa@kabelkultur.se>
2025-11-26 12:17:01 +0100

    Initialize environment loader during activation

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import { ExtensionController } from './services/extensionController';
+import { initializeEnvironment } from './utils/env';
 
 // Extension state
 let controller: ExtensionController | null = null;
@@ -8,6 +9,7 @@ let controller: ExtensionController | null = null;
  * Extension activation entry point.
  */
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
+	initializeEnvironment(context);
 	controller = new ExtensionController(context);
 	await controller.activate();
 }
