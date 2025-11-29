9fe130217ac0bd1a8d94d8c32b0d4a31130a0c0e
PA Ulander <pa@kabelkultur.se>
2025-11-29 14:05:18 +0100

    better wcag info in tooltips, add colored icons for pass vs fail

--- src/types/accessibility.ts
+++ src/types/accessibility.ts
@@ -1,12 +1,19 @@
 import * as vscode from 'vscode';
 
+export type AccessibilityOutcome = 'pass' | 'fail';
+
+export interface AccessibilityCheck {
+	label: string;
+	outcome: AccessibilityOutcome;
+}
+
 export interface AccessibilitySample {
 	label: string;
 	backgroundDescription: string;
 	backgroundColor: vscode.Color;
 	contrastRatio: number;
 	level: string;
-	passes: string[];
+	checks: AccessibilityCheck[];
 }
 
 export interface AccessibilityReport {
