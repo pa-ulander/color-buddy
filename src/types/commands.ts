36f851f68850f80c8e302932c517eafb07aae6ae
PA Ulander <pa@kabelkultur.se>
2025-11-30 02:38:26 +0100

    add ConvertColorCommandSource type, add SerializedRange interface, add ConvertColorCommandPayload interface

--- src/types/commands.ts
+++ src/types/commands.ts
@@ -1,10 +1,30 @@
 import type { ColorFormat } from './color';
 
 export type CopyColorCommandSource = 'hover' | 'statusBar' | 'command';
+export type ConvertColorCommandSource = CopyColorCommandSource;
+
+export interface SerializedPosition {
+	line: number;
+	character: number;
+}
+
+export interface SerializedRange {
+	start: SerializedPosition;
+	end: SerializedPosition;
+}
 
 export interface CopyColorCommandPayload {
 	value?: string;
 	format?: ColorFormat;
 	source?: CopyColorCommandSource;
 	showNotification?: boolean;
+}
+
+export interface ConvertColorCommandPayload {
+	uri: string;
+	range: SerializedRange;
+	normalizedColor: string;
+	originalText?: string;
+	format?: ColorFormat;
+	source?: ConvertColorCommandSource;
 }
